38d411937b87463bea8cdd0c80e8e6b9a5aff5ef
Ben Lumley <benbuildsstuff@gmail.com>
2013-11-16 12:29:05 +0000

    Updated folder names

--- testimonials.php
+++ testimonials.php
@@ -72,7 +72,7 @@ function dijitul_scripts() {
 
 	if(is_admin()) return false;
 
-	wp_enqueue_script('slider', plugins_url('/dijitul_testimonials/js/slider.js'));
+	wp_enqueue_script('slider', plugins_url('/fancy-testimonials/js/slider.js'));
 
 	wp_enqueue_script('jquery.cycle.all', plugins_url('/dijitul_testimonials/js/jquery.cycle.all.js'));
 
@@ -86,7 +86,7 @@ function dijitul_styles()
 
 { 
 
-  wp_register_style( 'custom-style', plugins_url('/dijitul_testimonials/css/custom-style.css'));
+  wp_register_style( 'custom-style', plugins_url('/fancy-testimonials/css/custom-style.css'));
 
 
 
